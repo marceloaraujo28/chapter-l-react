39e3d0d0f8f46b9a075c226b264b3863dfc3f0dd
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2023-01-02 22:49:16 -0300

    feat: create comment component

---
+++ src/components/Comment.tsx
@@ -0,0 +1,55 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
+import { Avatar } from "./Avatar";
+import styles from "./Comment.module.css";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
+  }
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/marceloaraujo28.png"
+        alt=""
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Marcelo Araújo</strong>
+              <time title="11 de Maio às 08:13:30" dateTime="2022-05-11 08:13h">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button onClick={handleDeleteComment} title="Deletar cometário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
